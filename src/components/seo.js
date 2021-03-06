7e5173427ec8e564ad310dc0f59e2a75d9a99978
mubarak96-max <mubarakmmm5@gmail.com>
2021-03-06 12:34:58 +0300

    cms

--- src/components/seo.js
+++ src/components/seo.js
@@ -31,7 +31,7 @@ function SEO({ description, title, keywords }) {
     <Helmet htmlAttributes={{lang:"en"}} title={title}>
       <meta name="description" content={description}/>
       <meta name="keywords" content={keywords}/>
-
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     </Helmet>
   )
 }
