78129c00d62d681633cfccdcec929d9a9b610333
mubarak96-max <mubarakmmm5@gmail.com>
2021-03-07 01:57:41 +0300

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -32,5 +31,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-cms`
   ],
 }
