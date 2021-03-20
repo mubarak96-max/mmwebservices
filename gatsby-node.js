faecb97da43686d4ae70e31a67d5f8878bfa05de
mubarak96-max <mubarakmmm5@gmail.com>
2021-03-20 12:15:49 +0300

    i

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,35 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it 
+// You can delete this file if you're not using it
+
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const pages = await graphql(`
+    {
+      allPrismicPost {
+        edges {
+          node {
+            id
+            uid
+          }
+        }
+      }
+    }
+  `)
+
+  const template = path.resolve("src/templates/post.js")
+
+  pages.data.allPrismicPost.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.uid}`,
+      component: template,
+      context: {
+        uid: edge.node.uid,
+      },
+    })
+  })
+}
